76fe4344852442fab45fe2b57c57f46d6807ea7d
Jaedson Silva <contacanal798@gmail.com>
2022-05-02 21:09:59 -0300

    adding custom print to log

---
+++ lprint.py
@@ -0,0 +1,16 @@
+def print_loading(message: str):
+    print(f'\033[1;33m[ LOADING ]\033[m \033[1;37m{message}\033[m')
+
+
+def print_sucess(message: str):
+    print(f'\033[1;32m[ SUCESS  ]\033[m \033[1;37m{message}\033[m')
+
+
+def print_error(message: str):
+    print(f'\033[1;31m[ ERROR   ]\033[m \033[1;37m{message}\033[m')
+
+
+if __name__ == '__main__':
+    print_loading('Loading wordlist')
+    print_sucess('Wordlist loaded')
+    print_error('Error to load wordlist')
